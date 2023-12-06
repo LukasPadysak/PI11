45ea756954a31713b87950a4ee33347567a2e01b
Lukáš Padyšák <lukaspadysak195@gmail.com>
2023-12-06 11:07:59 +0100

    santa.py

--- 13-tkinter_3/santa.py
+++ 13-tkinter_3/santa.py
@@ -49,6 +49,23 @@ while True:
 
 
 
+canvas = tkinter.Canvas(width = canvas_width,height = canvas_height)
+image_santa = tkinter.PhotoImage(file = "santa.png.png")
+santa = canvas.create_image(santa_y,santa_x, image = image_santa)
+canvas.pack()
+while True:
+    canvas.update()
+    time.sleep(0.01)
+    canvas.move(santa,0,santa_posun)
+    santa_y = santa + santa_posun
+    if santa_x == canvas_width- 64:
+        canvas.delete(santa)
+        santa_x = 64
+        santa = canvas.create_image(santa_x, santa_x, image=image_santa)
+
+
+
+
 
 
 
